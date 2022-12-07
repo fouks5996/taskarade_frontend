e389d4a77398c02dedef1cd5acca88717a94d02a
foucauld <foucauldbeaumont@yahoo.fr>
2022-12-07 14:19:06 +0100

    dd

--- pages/project/[id]/widget/[pid]/index.jsx
+++ pages/project/[id]/widget/[pid]/index.jsx
@@ -1,6 +1,4 @@
 import { unstable_getServerSession } from "next-auth";
-import { useSession } from "next-auth/react";
-import { queryTypes, SessionProps } from "next/app";
 import { useRouter } from "next/router";
 import React from "react";
 import Layout from "../../../../../components/Layout/Layout";
@@ -8,25 +6,18 @@ import Loader from "../../../../../components/Loader/Loader";
 import Notes from "../../../../../components/Widgets/Notes/Notes";
 import Tasks from "../../../../../components/Widgets/Tasks/Tasks";
 import Tickets from "../../../../../components/Widgets/Tickets/Tickets";
-import { useCurrentProject } from "../../../../../services/api/project";
 import { useCurrentWidget } from "../../../../../services/api/widget";
 import { authOptions } from '../../../../api/auth/[...nextauth]'
 
 
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
+
 	const session = await unstable_getServerSession(
 		context.req,
 		context.res,
 		authOptions
 	);
-	const currentUserID = session.id
-	const widgetID = parseInt(context.params.pid);
-	const projectID = parseInt(context.params.id);
-
-	context.res.setHeader(
-		'Cache-Control',
-		'public, s-maxage=10, stale-while-revalidate=60'
-  )
+	const currentUserID = session.id 
 
 	const options= {
 		method: 'GET',
@@ -34,42 +25,48 @@ export async function getStaticProps(context) {
 			Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`
 		}
 	}
+	const widgetID = parseInt(context.params.pid);
 
-	const dataP = await fetch(`${process.env.STRAPI_URL}/api/getssr-project/${projectID}`, options)
+	context.res.setHeader(
+		'Cache-Control',
+		'public, s-maxage=10, stale-while-revalidate=60'
+  )
+
+	const dataP = await fetch(`${process.env.STRAPI_URL}/api/getssr-widget/${widgetID}`, options)
 	const resP = await dataP.json()
-	const dataW = await fetch(`${process.env.STRAPI_URL}/api/getssr-widget/${widgetID}`, options)
-	const resW = await dataW.json()
 
-
-	if ((resW[0]?.widget_creator.id !== currentUserID && collaborationsIsTrue() === false) || (resW.length === 0 || resP.length === 0)) {
+ 	if ((resP[0]?.widget_creator.id !== currentUserID && collaborationsIsTrue() === false) || (resP.length === 0 )) {
 		return {
-			notFound: true,
+			redirect:{
+				destination: '/404',
+				permanent: false,
+			},
 		}
 	 }
 
 	function collaborationsIsTrue() {
-		return resP[0].collaborations.some(
+		return resP[0]?.project.collaborations.some(
 			(collaboration) =>
 				collaboration.collaborator.id === currentUserID
 		);
-	}
+	} 
 
 	return { 
 		props: {
-			widgetData: resW,
-			projectData: resP
+			widgetData: resP[0]
 		},
-		revalidate: 5, 
 	};
 }
 
-export default function Index({ widgetData, projectData }) {
-/* 	const router = useRouter();
-	const { pid } = router.query ;
-	const { widget, isWidgetLoading } = useCurrentWidget(parseInt(pid));
- */
 
-/* 
+export default function Index({widgetData }) {
+/*  	const router = useRouter();
+	const { pid } = router.query ;
+	const { widget, isWidgetLoading } = useCurrentWidget(parseInt(pid)); */
+
+	console.log(widgetData);
+
+/*  
 	if (isWidgetLoading)
 		return (
 			<Layout title='Loading'>
@@ -77,13 +74,11 @@ export default function Index({ widgetData, projectData }) {
 					<Loader type='spin' height={40} width={40} />{" "}
 				</div>
 			</Layout>
-		); */
+		);  */
 
-		console.log(widgetData);
-
-	switch (widgetData[0].widget.id) {
+	switch (widgetData.widget.id) {
 		case 1:
-/* 			if (widgetData[0].notes.length !== 0) {
+/*  			if (widget.data?.attributes.notes.data.length !== 0) {
 				const today = new Date();
 				const closest =
 					widget.data?.attributes.notes.data.reduce((a, b) =>
@@ -98,13 +93,13 @@ export default function Index({ widgetData, projectData }) {
 						<Notes maxId={closest.id} />
 					</Layout>
 				);
-			} else { */
+			} else {  */
 				return (
 					<Layout title={"Notes"}>
 						<Notes maxId={0} />
 					</Layout>
 				);
-/* 			} */
+ 			/* }  */
 		case 2:
 			return (
 				<Layout title={"Tasks"}>
